5df4e886ef3dc0163eb0437496ee92379ecc3f12
Fran Barrena <francisco@kyso.io>
2022-02-06 19:35:58 +0100

    another weird auto-importation of vscode...

--- src/modules/invitations/invitations.module.ts
+++ src/modules/invitations/invitations.module.ts
@@ -1,7 +1,7 @@
 import { DynamicModule } from '@nestjs/common'
 import { InvitationsController } from './invitations.controller'
 import { createProvider, InvitationsService } from './invitations.service'
-import { InvitationsMongoProvider } from './providers/Invitations-mongo.provider'
+import { InvitationsMongoProvider } from './providers/invitations-mongo.provider'
 
 export class InvitationsModule {
     static forRoot(): DynamicModule {
