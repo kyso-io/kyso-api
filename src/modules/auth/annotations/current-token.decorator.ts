2dcebf3ded718f2a40fab42071ecbcafa9716d2e
Fran Barrena <francisco@kyso.io>
2022-02-09 03:16:28 +0100

    testing currenttoken

--- src/modules/auth/annotations/current-token.decorator.ts
+++ src/modules/auth/annotations/current-token.decorator.ts
@@ -2,6 +2,6 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common'
 
 export const CurrentToken = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest()
-    console.log(request)
+    console.log(request.headers.authorization)
     return request.token
 })
