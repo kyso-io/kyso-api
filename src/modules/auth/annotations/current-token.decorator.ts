ed913961842f589cc3200f409b8a5d405a8db904
Fran Barrena <francisco@kyso.io>
2022-02-09 03:12:18 +0100

    debuigging in k8s

--- src/modules/auth/annotations/current-token.decorator.ts
+++ src/modules/auth/annotations/current-token.decorator.ts
@@ -2,6 +2,6 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common'
 
 export const CurrentToken = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest()
-
+    console.log(request)
     return request.token
 })
