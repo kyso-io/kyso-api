6534328e0fa6770f4ce2184edc6ae53d47fcbdfa
Daniel Andújar <daniandujar@gmail.com>
2022-02-18 15:30:52 +0100

    feat: bitbutcket repositories connection

--- src/modules/bitbucket-repos/bitbucket-repos.controller.ts
+++ src/modules/bitbucket-repos/bitbucket-repos.controller.ts
@@ -1,24 +1,24 @@
-import { Repository } from '@kyso-io/kyso-model'
-import { Controller, Get, Param, Query, Req } from '@nestjs/common'
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
+import { GithubFileHash, GithubRepository, LoginProviderEnum, NormalizedResponseDTO, Repository, Token, User, UserAccount } from '@kyso-io/kyso-model'
+import { Controller, Get, Param, PreconditionFailedException, Query } from '@nestjs/common'
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger'
+import { ApiNormalizedResponse } from '../../decorators/api-normalized-response'
+import { Autowired } from '../../decorators/autowired'
 import { GenericController } from '../../generic/controller.generic'
+import { CurrentToken } from '../auth/annotations/current-token.decorator'
+import { UsersService } from '../users/users.service'
 import { BitbucketReposProvider } from './providers/bitbucket-repo.provider'
 
 @ApiTags('repos/bitbucket')
 @Controller('repos/bitbucket')
 export class BitbucketReposController extends GenericController<Repository> {
-    constructor(private readonly reposService: BitbucketReposProvider) {
+    @Autowired({ typeName: 'UsersService' })
+    private usersService: UsersService
+
+    constructor(private readonly bitbucketReposProvider: BitbucketReposProvider) {
         super()
     }
 
     assignReferences(repo: Repository) {
-        // repo.self_url = HateoasLinker.createRef(`/repos/github/${repo.owner}/${repo.name}`)
-        // repo.tree_url = HateoasLinker.createRef(`/repos/github/${repo.owner}/${repo.name}/${repo.default_branch}/tree`)
-
-        /* TODO: Does not appear in the documentation... it's correct?
-        if (repo.report) repo.reportUrl = HateoasLinker.createRef(`/${repo.report}`)
-        delete repo.report
-        */
         return repo
     }
 
@@ -31,34 +31,28 @@ export class BitbucketReposController extends GenericController<Repository> {
         description: `Search results matching criteria`,
         type: Repository,
     })
-    async getRepos(@Query('filter') filter, @Query('page') page, @Query('per_page') perPage, @Req() req) {
-        // TODO
-    }
-
-    @Get('/:repoOwner/:repoName')
-    @ApiOperation({
-        summary: `Fetch data for a repository, after specifying the owner and the name of the repository`,
-    })
-    @ApiResponse({
-        status: 200,
-        description: `The data of the specified repository`,
-        type: Repository,
-    })
-    async getRepo(@Param('repoOwner') repoOwner: string, @Param('repoName') repoName: string, @Req() req) {
-        // TODO
-    }
-
-    @Get('/:repoOwner/:repoName/:branch/tree')
-    @ApiOperation({
-        summary: `Get the tree of a specific repository`,
-    })
-    @ApiResponse({
-        status: 200,
-        description: `The data of the specified repository`,
-        type: Repository,
-    })
-    async getRepoTree(@Param('repoOwner') repoOwner: string, @Param('repoName') repoName: string, @Param('branch') branch: string, @Req() req) {
-        // TODO
+    async getRepos(
+        @CurrentToken() token: Token,
+        @Query('filter') filter,
+        @Query('page') page,
+        @Query('per_page') perPage,
+    ): Promise<NormalizedResponseDTO<GithubRepository[]>> {
+        const user: User = await this.usersService.getUserById(token.id)
+        const userAccount: UserAccount = user.accounts.find((account: UserAccount) => account.type === LoginProviderEnum.BITBUCKET)
+        if (!userAccount) {
+            throw new PreconditionFailedException('User does not have a bitbucket account')
+        }
+        const data: any = await this.bitbucketReposProvider.getWorkspaces(userAccount.username, userAccount.accessToken, 1, 100)
+        let workspaces: string[] = []
+        if (data?.values && Array.isArray(data.values) && data.values.length > 0) {
+            workspaces = data.values.map((workspace: any) => workspace.slug)
+        }
+        const repos: GithubRepository[] = await Promise.all(
+            workspaces.map((workspace: string) =>
+                this.bitbucketReposProvider.searchRepos(userAccount.username, userAccount.accessToken, workspace, filter, page, perPage),
+            ),
+        )
+        return new NormalizedResponseDTO(repos)
     }
 
     @Get('/user')
@@ -67,9 +61,93 @@ export class BitbucketReposController extends GenericController<Repository> {
     })
     @ApiResponse({
         status: 200,
-        description: `The data of the specified repository` /* type: GithubAccount */,
+        description: `The data of the specified repository`,
     })
-    async getAuthenticatedUser() {
-        // TODO
+    async getAuthenticatedUser(@CurrentToken() token: Token): Promise<NormalizedResponseDTO<any>> {
+        try {
+            const user: User = await this.usersService.getUserById(token.id)
+            const userAccount: UserAccount = user.accounts.find((account: UserAccount) => account.type === LoginProviderEnum.BITBUCKET)
+            if (!userAccount) {
+                throw new PreconditionFailedException('User does not have a bitbucket account')
+            }
+            const bitbucketAccount: any = await this.bitbucketReposProvider.getUser(userAccount.username, userAccount.accessToken)
+            return new NormalizedResponseDTO(bitbucketAccount)
+        } catch (e) {
+            console.log(e)
+            return new NormalizedResponseDTO(null)
+        }
+    }
+
+    @Get('/repository')
+    @ApiOperation({
+        summary: `Get a single repository given name`,
+        description: `Fetch data for a repository, after specifying the owner and the name of the repository`,
+    })
+    @ApiNormalizedResponse({
+        status: 200,
+        description: `The data of the specified repository`,
+        type: Repository,
+    })
+    async getRepo(@CurrentToken() token: Token, @Query('name') name: string): Promise<NormalizedResponseDTO<GithubRepository>> {
+        try {
+            if (!name || name.length === 0) {
+                throw new PreconditionFailedException('Repository name is required')
+            }
+            const user: User = await this.usersService.getUserById(token.id)
+            const userAccount: UserAccount = user.accounts.find((account: UserAccount) => account.type === LoginProviderEnum.BITBUCKET)
+            if (!userAccount) {
+                throw new PreconditionFailedException('User does not have a bitbucket account')
+            }
+            const repository: GithubRepository = await this.bitbucketReposProvider.getRepo(userAccount.username, userAccount.accessToken, name)
+            return new NormalizedResponseDTO(repository)
+        } catch (e) {
+            return new NormalizedResponseDTO(null)
+        }
+    }
+
+    @Get('/:branch/tree')
+    @ApiOperation({
+        summary: `Explore a repository tree`,
+        description: `Get the tree of a specific repository`,
+    })
+    @ApiParam({
+        name: 'branch',
+        required: true,
+        description: 'Branch to fetch content from. Accepts slashes.',
+        schema: { type: 'string' },
+    })
+    @ApiNormalizedResponse({
+        status: 200,
+        description: `The data of the specified repository`,
+        type: Repository,
+    })
+    async getRepoTree(
+        @CurrentToken() token: Token,
+        @Param('branch') branch: string,
+        @Query('name') name: string,
+        @Param('path') path: string,
+    ): Promise<NormalizedResponseDTO<{ nextPageCode: string; data: GithubFileHash[] }>> {
+        try {
+            if (!name || name.length === 0) {
+                throw new PreconditionFailedException('Repository name is required')
+            }
+            const user: User = await this.usersService.getUserById(token.id)
+            const userAccount: UserAccount = user.accounts.find((account: UserAccount) => account.type === LoginProviderEnum.BITBUCKET)
+            if (!userAccount) {
+                throw new PreconditionFailedException('User does not have a bitbucket account')
+            }
+            const tree: { nextPageCode: string; data: GithubFileHash[] } = await this.bitbucketReposProvider.getRootFilesAndFoldersByCommit(
+                userAccount.username,
+                userAccount.accessToken,
+                name,
+                branch,
+                path,
+                null,
+            )
+            return new NormalizedResponseDTO(tree)
+        } catch (e) {
+            console.log(e)
+            return new NormalizedResponseDTO(null)
+        }
     }
 }
--- src/modules/bitbucket-repos/providers/bitbucket-repo.provider.ts
+++ src/modules/bitbucket-repos/providers/bitbucket-repo.provider.ts
@@ -1,5 +1,5 @@
 import { GithubRepository } from '@kyso-io/kyso-model'
-import { Injectable, Scope } from '@nestjs/common'
+import { Injectable } from '@nestjs/common'
 import { NotFoundError } from '../../../helpers/errorHandling'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const axios = require('axios').default
@@ -18,36 +18,66 @@ const repoMapFunction = (repo: any): GithubRepository => ({
 
 const DEFAULT_PER_PAGE = 20
 
-@Injectable({ scope: Scope.REQUEST })
+@Injectable()
 export class BitbucketReposProvider {
-    authenticationMethod: string
-    authorizationHeader: string
-
-    public withUserAndAppPassword(username: string, appPassword: string): void {
-        this.authenticationMethod = 'basic-username-app-password'
-        // build basic authentication
-        const buffer = Buffer.from(`${username}:${appPassword}`)
-        this.authorizationHeader = `Basic ${buffer.toString('base64')}`
+    constructor() {
+        // const username = 'dani-kyso'
+        // const password = 'D6GjpNwngz9A3t7XjZwq'
+        // this.getWorkspaces(username, password, 1, DEFAULT_PER_PAGE)
+        //     .then((res) => console.log(res))
+        //     .catch((err) => console.log(err))
+        // this.getAllRepos(username, password)
+        //     .then((result) => console.log(result))
+        //     .catch((err) => console.log(err))
+        // this.getRepo(username, password, 'dani-kyso/dummy-project')
+        //     .then((result) => console.log(result))
+        //     .catch((err) => console.log(err))
+        // this.searchRepos(username, password, 'dani-kyso', 'name~"dummy"', 1, DEFAULT_PER_PAGE)
+        //     .then((result) => console.log(result))
+        //     .catch((err) => console.log(err))
+        // this.getBranches(username, password, 'dani-kyso/dummy-project', 1, DEFAULT_PER_PAGE)
+        //     .then((result) => console.log(result))
+        //     .catch((err) => console.log(err))
+        // this.getCommits(username, password, 'dani-kyso/dummy-project', 'master', 1, DEFAULT_PER_PAGE)
+        //     .then((result) => console.log(result))
+        //     .catch((err) => console.log(err))
+        // this.getRootFilesAndFolders(username, password, 'dani-kyso/dummy-project')
+        //     .then((result) => console.log(result))
+        //     .catch((err) => console.log(err))
+        // this.getRootFilesAndFoldersByCommit(username, password, 'dani-kyso/dummy-project', '7aac65f1606c246ee074bb9a579cf737f3288e95', 'data/skus', null)
+        //     .then((result) => console.log(result))
+        //     .catch((err) => console.log(err))
+        // this.getFileContent(username, password, 'dani-kyso/dummy-project', '7aac65f1606c246ee074bb9a579cf737f3288e95', 'index.js')
+        //     .then((result) => console.log(Buffer.from(result).toString()))
+        //     .catch((err) => console.log(err))
+        // this.getUser(username, password)
+        //     .then((result) => console.log(result))
+        //     .catch((err) => console.log(err))
     }
 
-    public async getRepos(workspace, page, perPage): Promise<any[]> {
+    private withUserAndAppPassword(username: string, appPassword: string): string {
+        const buffer = Buffer.from(`${username}:${appPassword}`)
+        return `Basic ${buffer.toString('base64')}`
+    }
+
+    private async getRepos(username: string, password: string, workspace: string, page: number, perPage: number): Promise<any[]> {
         const res = await axios.get(`${process.env.BITBUCKET_API}/repositories/${workspace}?page=${page}&pagelen=${perPage}`, {
-            headers: { Authorization: this.authorizationHeader },
+            headers: { Authorization: this.withUserAndAppPassword(username, password) },
         })
         return res.data.values.map(repoMapFunction)
     }
 
-    public async getAllRepos(): Promise<any[]> {
+    public async getAllRepos(username: string, password: string): Promise<any[]> {
         let page = 1
         let workspaces = null
         let result = []
         do {
-            workspaces = await this.getWorkspaces(page, DEFAULT_PER_PAGE)
+            workspaces = await this.getWorkspaces(username, password, page, DEFAULT_PER_PAGE)
             let paginatedResponse = null
             for (const workspace of workspaces.values) {
                 let repositoryPage = 1
                 do {
-                    paginatedResponse = await this.getRepos(workspace.slug, repositoryPage, DEFAULT_PER_PAGE)
+                    paginatedResponse = await this.getRepos(username, password, workspace.slug, repositoryPage, DEFAULT_PER_PAGE)
                     repositoryPage++
                     result = [...result, ...paginatedResponse]
                 } while (paginatedResponse.length > 0)
@@ -57,30 +87,41 @@ export class BitbucketReposProvider {
         return result
     }
 
-    public async getWorkspaces(page, perPage): Promise<any> {
+    public async getWorkspaces(username: string, password: string, page: number, perPage: number): Promise<any> {
         const res = await axios.get(`${process.env.BITBUCKET_API}/workspaces?page=${page}&pagelen=${perPage}`, {
-            headers: { Authorization: this.authorizationHeader },
+            headers: { Authorization: this.withUserAndAppPassword(username, password) },
         })
         return res.data
     }
 
-    public async getRepo(workspace, name): Promise<any> {
-        const res = await axios.get(`${process.env.BITBUCKET_API}/repositories/${workspace}/${name}`, {
-            headers: { Authorization: this.authorizationHeader },
+    public async getRepo(username: string, password: string, fullName: string): Promise<any> {
+        const res = await axios.get(`${process.env.BITBUCKET_API}/repositories/${fullName}`, {
+            headers: { Authorization: this.withUserAndAppPassword(username, password) },
         })
         return repoMapFunction(res.data)
     }
 
-    public async searchRepos(filter, workspace, page, perPage): Promise<any> {
-        const res = await axios.get(`${process.env.BITBUCKET_API}/repositories/${workspace}?q=${filter}&page=${page}&pagelen=${perPage}`, {
-            headers: { Authorization: this.authorizationHeader },
+    // https://developer.atlassian.com/cloud/bitbucket/rest/intro/#filtering
+    public async searchRepos(username: string, password: string, workspace: string, filter: string, page: number, perPage: number): Promise<any> {
+        let url = `${process.env.BITBUCKET_API}/repositories/${workspace}?`
+        if (filter) {
+            url += `q=${filter}`
+        }
+        if (page) {
+            url += `&page=${page}`
+        }
+        if (perPage) {
+            url += `&pagelen=${perPage}`
+        }
+        const res = await axios.get(url, {
+            headers: { Authorization: this.withUserAndAppPassword(username, password) },
         })
         return res.data.values.map(repoMapFunction)
     }
 
-    public async getBranches(workspace, repo, page, perPage): Promise<any> {
-        const res = await axios.get(`${process.env.BITBUCKET_API}/repositories/${workspace}/${repo}/refs/branches?page=${page}&pagelen=${perPage}`, {
-            headers: { Authorization: this.authorizationHeader },
+    public async getBranches(username: string, password: string, fullName: string, page: number, perPage: number): Promise<any> {
+        const res = await axios.get(`${process.env.BITBUCKET_API}/repositories/${fullName}/refs/branches?page=${page}&pagelen=${perPage}`, {
+            headers: { Authorization: this.withUserAndAppPassword(username, password) },
         })
         return res.data.values.map((branch) => ({
             name: branch.name,
@@ -88,9 +129,9 @@ export class BitbucketReposProvider {
         }))
     }
 
-    public async getCommits(workspace, repo, branch, page, perPage): Promise<any> {
-        const res = await axios.get(`${process.env.BITBUCKET_API}/repositories/${workspace}/${repo}/commits/${branch}?page=${page}&pagelen=${perPage}`, {
-            headers: { Authorization: this.authorizationHeader },
+    public async getCommits(username: string, password: string, fullName: string, branch: string, page: number, perPage: number): Promise<any> {
+        const res = await axios.get(`${process.env.BITBUCKET_API}/repositories/${fullName}/commits/${branch}?page=${page}&pagelen=${perPage}`, {
+            headers: { Authorization: this.withUserAndAppPassword(username, password) },
         })
         return res.data.values.map((elem) => ({
             sha: elem.hash,
@@ -108,8 +149,8 @@ export class BitbucketReposProvider {
         return text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi)
     }
 
-    public async getRootFilesAndFolders(workspace, repo, pageCode): Promise<any> {
-        return this.getRootFilesAndFoldersByCommit(workspace, repo, null, pageCode)
+    public async getRootFilesAndFolders(username: string, password: string, fullName: string): Promise<any> {
+        return this.getRootFilesAndFoldersByCommit(username, password, fullName, null, null, null)
     }
 
     /**
@@ -122,17 +163,27 @@ export class BitbucketReposProvider {
      *                     as a code, and not as an incremental number.
      * @returns
      */
-    public async getRootFilesAndFoldersByCommit(workspace, repo, commit, pageCode): Promise<any> {
-        let requestUrl = `${process.env.BITBUCKET_API}/repositories/${workspace}/${repo}/src/`
+    public async getRootFilesAndFoldersByCommit(
+        username: string,
+        password: string,
+        fullName: string,
+        commit: string,
+        folder: string,
+        pageCode: number,
+    ): Promise<any> {
+        let requestUrl = `${process.env.BITBUCKET_API}/repositories/${fullName}/src/`
         if (commit) {
             requestUrl = requestUrl + `${commit}/`
+        }
+        if (folder && folder.length > 0) {
+            requestUrl = requestUrl + `${folder}/`
         }
         if (pageCode) {
             requestUrl = requestUrl + `?page=${pageCode}`
         }
         // The last slash (/) is important, without it... does not works...
         const res = await axios.get(requestUrl, {
-            headers: { Authorization: this.authorizationHeader },
+            headers: { Authorization: this.withUserAndAppPassword(username, password) },
         })
         const filterData = (obj) => ({
             type: obj.type,
@@ -150,12 +201,13 @@ export class BitbucketReposProvider {
         }
     }
 
-    public async getFileContent(workspace, repo, commit, filePath): Promise<any> {
+    public async getFileContent(username: string, password: string, fullName: string, commit: string, filePath: string): Promise<any> {
         try {
-            const res = await axios.get(`${process.env.BITBUCKET_API}/repositories/${workspace}/${repo}/src/${commit}/${filePath}`, {
-                headers: { Authorization: this.authorizationHeader },
+            const res = await axios.get(`${process.env.BITBUCKET_API}/repositories/${fullName}/src/${commit}/${filePath}`, {
+                headers: { Authorization: this.withUserAndAppPassword(username, password) },
+                transformResponse: [(data) => data],
             })
-            return Buffer.from(res.data).toString('base64')
+            return Buffer.from(res.data, 'utf-8')
         } catch (err) {
             if (err.status === 404) {
                 throw new NotFoundError({
@@ -166,9 +218,9 @@ export class BitbucketReposProvider {
         }
     }
 
-    public async getUser(): Promise<any> {
+    public async getUser(username: string, password: string): Promise<any> {
         const res = await axios.get(`${process.env.BITBUCKET_API}/user`, {
-            headers: { Authorization: this.authorizationHeader },
+            headers: { Authorization: this.withUserAndAppPassword(username, password) },
         })
         return res.data
     }
