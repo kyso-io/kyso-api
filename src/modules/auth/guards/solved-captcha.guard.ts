8914f2febaaee732e715e6c9b9f11905aaf8f303
Fran Barrena <francisco@kyso.io>
2022-04-13 19:45:48 +0200

    Solved Captcha Guard was not taking into account RECAPTCHA2_ENABLE

--- src/modules/auth/guards/solved-captcha.guard.ts
+++ src/modules/auth/guards/solved-captcha.guard.ts
@@ -1,5 +1,6 @@
-import { Token, User } from '@kyso-io/kyso-model'
+import { KysoSettingsEnum, Token, User } from '@kyso-io/kyso-model'
 import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common'
+import { KysoSettingsService } from 'src/modules/kyso-settings/kyso-settings.service'
 import { Autowired } from '../../../decorators/autowired'
 import { UsersService } from '../../users/users.service'
 import { AuthService } from '../auth.service'
@@ -12,8 +13,14 @@ export class SolvedCaptchaGuard implements CanActivate {
     @Autowired({ typeName: 'UsersService' })
     private readonly usersService: UsersService
 
+    @Autowired({ typeName: 'KysoSettingsService' })
+    private readonly kysoSettingsService: KysoSettingsService
+
     async canActivate(context: ExecutionContext): Promise<boolean> {
         const request = context.switchToHttp().getRequest()
+
+        const captchaEnabled = await this.kysoSettingsService.getValue(KysoSettingsEnum.RECAPTCHA2_ENABLED) === 'true' ? true : false;
+
         if (!request.headers.hasOwnProperty('authorization') || request.headers['authorization'] === null || request.headers['authorization'] === '') {
             throw new ForbiddenException('Missing authorization header')
         }
@@ -22,12 +29,15 @@ export class SolvedCaptchaGuard implements CanActivate {
             throw new ForbiddenException('Invalid jwt token')
         }
         const user: User = await this.usersService.getUserById(tokenPayload.id)
+        
         if (!user) {
             throw new ForbiddenException('User not found with this jwt token')
         }
-        if (user.show_captcha) {
+
+        if (captchaEnabled && user.show_captcha) {
             throw new ForbiddenException('Captcha not solved')
         }
+
         return true
     }
 }
