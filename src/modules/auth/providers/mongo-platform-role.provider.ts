0d753c799dda2a86b4026c1cbd4f1f329b7e6a16
Daniel Andújar Lorenzo <daniandujar@gmail.com>
2022-04-28 15:37:09 +0200

    feat: update permissions of predefined roles

--- src/modules/auth/providers/mongo-platform-role.provider.ts
+++ src/modules/auth/providers/mongo-platform-role.provider.ts
@@ -1,3 +1,4 @@
+import { KysoRole } from '@kyso-io/kyso-model'
 import { Injectable, Logger } from '@nestjs/common'
 import { db } from '../../../main'
 import { MongoProvider } from '../../../providers/mongo.provider'
@@ -6,9 +7,10 @@ import { PlatformRole } from '../../../security/platform-roles'
 @Injectable()
 export class PlatformRoleMongoProvider extends MongoProvider<any> {
     version = 1
-    
+
     constructor() {
         super('PlatformRole', db)
+        this.checkRoles()
     }
 
     async populateMinimalData() {
@@ -26,5 +28,33 @@ export class PlatformRoleMongoProvider extends MongoProvider<any> {
 
         Logger.log(`Creating organization-admin role`)
         await this.create(PlatformRole.ORGANIZATION_ADMIN_ROLE)
+    }
+
+    async checkRoles(): Promise<void> {
+        const roles: KysoRole[] = [
+            PlatformRole.PLATFORM_ADMIN_ROLE,
+            PlatformRole.TEAM_ADMIN_ROLE,
+            PlatformRole.TEAM_CONTRIBUTOR_ROLE,
+            PlatformRole.TEAM_READER_ROLE,
+            PlatformRole.ORGANIZATION_ADMIN_ROLE,
+        ]
+        for (const role of roles) {
+            const rolesDb: KysoRole[] = await this.read({
+                filter: {
+                    name: role.name,
+                },
+            })
+            if (rolesDb.length === 0) {
+                continue
+            }
+            const roleDb: KysoRole = rolesDb[0]
+            await this.updateOne(
+                { _id: this.toObjectId(roleDb.id) },
+                {
+                    $set: { permissions: role.permissions },
+                },
+            )
+            Logger.log(`Updated role ${role.name}`, PlatformRoleMongoProvider.name)
+        }
     }
 }
