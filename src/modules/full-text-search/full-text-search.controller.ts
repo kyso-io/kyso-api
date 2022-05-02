2bf3f80bd6f5be125dcd3b28899748956eb3e9f8
Fran Barrena <francisco@kyso.io>
2022-05-02 20:08:38 +0200

    fake results

--- src/modules/full-text-search/full-text-search.controller.ts
+++ src/modules/full-text-search/full-text-search.controller.ts
@@ -132,8 +132,82 @@ export class FullTextSearchController {
                 // silent it
             }
         }
+
+        // start Hack requested by @kyle for a demo ;P       
+        finalTagsSet.add("churn")
+        finalTagsSet.add("engagement")
+        finalTagsSet.add("b2b-chorts")
+
+        const fakeData: FullTextSearchResult[] = [];
         
+        fakeData.push(
+            new FullTextSearchResult(
+                "What is driving the purchase behaviour of Acme's customers?",
+                "It has been shown income has the strongest relationship with purchase behavior of customers. However, interesting insights about education and age along with age_group have still been noted. These insights would be very helpful to how this store markets deals to their customers and prices items, such as wine since higher income groups tend to dominate alcohol sales. There is also opportunity to increase market to the 18 to 35 and 71 and Older age groups to drive products sales.",
+                "acme/marketing/what-is-driving-the-purchase-behaviour-of-acmes-customers",
+                "report",
+                [],
+                "marketing", "acme", ["customer-patterns", "buyer-behaviour", "product-usage"],
+                "what-is-driving-the-purchase-behaviour-of-acmes-customers", 1, "marketing-data-analytics.ipynb", 91.312
+            )
+        )
+
+        fakeData.push(
+            new FullTextSearchResult(
+                "What is driving the purchase behaviour of Acme's customers?",
+                "Given that we're a SaaS company that charges per seat for team subscriptions, user engagement is super important for maintaining our high retention levels. Also, because we do charge per seat, by measuring the relationship between team size and various engagement metrics like views, posts, and other actions. Should we be focusing more time on smaller accounts or only on the big fish?",
+                "acme/product/plotting-account-activity-levels",
+                "report",
+                [],
+                "product", "acme", ["churn", "engagement", "b2b-chorts"],
+                "plotting-account-activity-levels", 1, "main.ipynb", 91.312
+            )
+        )
+
+        fakeData.push(
+            new FullTextSearchResult(
+                "Customer Survival Analysis 2021",
+                "Predictive analytics use churn prediction models that predict customer churn by assessing their propensity of risk to churn. Since these models generate a small prioritized list of potential defectors, they are effective at focusing customer retention marketing programs on the subset of the customer base who are most vulnerable to churn. In this project we aim to perform customer survival analysis and build a model which can predict customer churn. We also aim to build an app which can be used to understand why a specific customer would stop the service and to know his/her expected lifetime value.",
+                "acme/product/customer-survival-analysis-2021",
+                "report",
+                [],
+                "product", "acme", ["churn", "b2b", "survival-analysis"],
+                "customer-survival-analysis-2021", 3, "README.md", 91.312
+            )
+        )
+
+        fakeData.push(
+            new FullTextSearchResult(
+                "B2B App Cohort Analysis",
+                "It appears that the May 2016 cohort has decreased in revenue while the others may have had expansion offsetting churned revenue. The revenue retention visualization really provides more information about the 2016 customer cohort. All 12 months of cohort groups have had some churn or expansion. We would want to follow-up and figure out how customer success or onboarding (or account management) contributed to the April, June, August, September and November cohort expansion and bring those best practices to the rest of the customer base. Conversely, are there any lessons learned from the May cohort and can we turn these customers' usage around.",
+                "acme/product/b2b-app-cohort-analysis",
+                "report",
+                [],
+                "product", "acme", [],
+                "b2b-app-cohort-analysis", 1, "cohort_analysis.ipynb", 91.312
+            )
+        )
+
+        fakeData.push(
+            new FullTextSearchResult(
+                "How good is Acme at activating its customers and retaining high usage throughout the first 3 months after signup?",
+                "We are tasked to Perform Cohort and Recency Frequency and Monetary Value Analysis to understand the value derived from different customer segments. Further, we will divide customers in different cluster traits based on the analysis by using Unsupervised Learning Techniques.",
+                "acme/product/how-good-is-acme-at-activating-its-customers-and-retaining-high-usage-throughout-the-first-3-months-after-signup",
+                "report",
+                [],
+                "product", "acme", [],
+                "how-good-is-acme-at-activating-its-customers-and-retaining-high-usage-throughout-the-first-3-months-after-signup", 2, "MAIN.ipynb", 91.312
+            )
+        )
+
+        
+        searchResults.reports.teams.push("product")
+        searchResults.reports.teams.push("marketing")
+        searchResults.reports.organizations.push("acme")
+
+                
         searchResults.reports.results = censoredResults
+        searchResults.reports.results.splice(1,0, ...fakeData)
         searchResults.reports.tags = Array.from(finalTagsSet)
 
         return new NormalizedResponseDTO(searchResults, null);
