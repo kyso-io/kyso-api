9c431c7daad5f5c04532c001ebbb6125d525cd50
Fran Barrena <francisco@kyso.io>
2022-05-25 20:15:40 +0200

    not working, powered up to 5 again

--- src/modules/full-text-search/full-text-search.service.ts
+++ src/modules/full-text-search/full-text-search.service.ts
@@ -64,7 +64,7 @@ export class FullTextSearchService extends AutowiredService {
                                 content: {
                                     query: terms,
                                     operator: "or",
-                                    fuzziness: 3
+                                    fuzziness: 5
                                 }
                             }
                         },
