47442440ed7cb21704f229925b2e94ed9e2850be
Fran Barrena <francisco@kyso.io>
2023-02-15 15:52:03 +0100

    fix: added debugging messages to permissions guard

--- src/modules/auth/guards/permission.guard.ts
+++ src/modules/auth/guards/permission.guard.ts
@@ -43,12 +43,14 @@ export class PermissionsGuard implements CanActivate {
       const request = context.switchToHttp().getRequest();
 
       if (!request.headers.hasOwnProperty('authorization') || request.headers['authorization'] === null || request.headers['authorization'] === '') {
+        Logger.debug('No authorization header present');
         return false;
       }
 
       // Get the token
       const tokenPayload: Token = this.authService.evaluateAndDecodeTokenFromHeader(request.headers.authorization);
       if (!tokenPayload) {
+        Logger.debug('Token is invalid');
         return false;
       }
 
@@ -67,6 +69,9 @@ export class PermissionsGuard implements CanActivate {
       // Set token in request to get access in controller
       request.token = tokenPayload;
 
+      Logger.debug(`Header ${HEADER_X_KYSO_TEAM} is ${request.headers[HEADER_X_KYSO_TEAM]}`);
+      Logger.debug(`Header ${HEADER_X_KYSO_ORGANIZATION} is ${request.headers[HEADER_X_KYSO_ORGANIZATION]}`);
+
       const team = request.headers[HEADER_X_KYSO_TEAM];
       const organization = request.headers[HEADER_X_KYSO_ORGANIZATION];
 
@@ -83,6 +88,8 @@ export class PermissionsGuard implements CanActivate {
       }
 
       const permissionToActivateEndpoint = this.reflector.getAllAndOverride<any>(PERMISSION_KEY, [context.getHandler(), context.getClass()]);
+      Logger.debug(`Permissions to activate endpoint are ${permissionToActivateEndpoint}`);
+      Logger.debug(`User's permission are ${tokenPayload.permissions}`);
 
       return AuthService.hasPermissions(tokenPayload, permissionToActivateEndpoint, teamObject ? teamObject : null, organizationObject ? organizationObject : null);
     } catch (ex) {
