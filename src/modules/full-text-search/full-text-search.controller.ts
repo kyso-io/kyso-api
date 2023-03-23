7c7619b18caf84c0e84bc810ef104a2c54f1f589
Daniel Andújar <daniandujar@gmail.com>
2023-03-23 11:59:05 +0100

    feat: advances in full text search

--- src/modules/full-text-search/full-text-search.controller.ts
+++ src/modules/full-text-search/full-text-search.controller.ts
@@ -1,4 +1,14 @@
-import { ElasticSearchIndex, FullTextSearchDTO, FullTextSearchMetadata, FullTextSearchResultType, GlobalPermissionsEnum, NormalizedResponseDTO, Tag, Token } from '@kyso-io/kyso-model';
+import {
+  ElasticSearchIndex,
+  FullTextSearchAggregators,
+  FullTextSearchDTO,
+  FullTextSearchMetadata,
+  FullTextSearchResultType,
+  GlobalPermissionsEnum,
+  NormalizedResponseDTO,
+  Tag,
+  Token,
+} from '@kyso-io/kyso-model';
 import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { ApiNormalizedResponse } from '../../decorators/api-normalized-response';
@@ -9,7 +19,6 @@ import { Permission } from '../auth/annotations/permission.decorator';
 import { PermissionsGuard } from '../auth/guards/permission.guard';
 import { CommentsService } from '../comments/comments.service';
 import { DiscussionsService } from '../discussions/discussions.service';
-import { KysoSettingsService } from '../kyso-settings/kyso-settings.service';
 import { ReportsService } from '../reports/reports.service';
 import { UsersService } from '../users/users.service';
 import { FullTextSearchService } from './full-text-search.service';
@@ -24,9 +33,6 @@ export class FullTextSearchController {
 
   @Autowired({ typeName: 'UsersService' })
   private usersService: UsersService;
-
-  @Autowired({ typeName: 'KysoSettingsService' })
-  private kysoSettingsService: KysoSettingsService;
 
   @Autowired({ typeName: 'DiscussionsService' })
   private discussionsService: DiscussionsService;
@@ -47,26 +53,49 @@ export class FullTextSearchController {
   @ApiQuery({ name: 'page', required: true, description: "Result's page to be retrieved" })
   @ApiQuery({ name: 'type', required: true, description: 'Type of object to search for', example: 'report, discussion, comment, member' })
   @ApiQuery({ name: 'perPage', required: false, description: 'Number of results per page. 100 if not set' })
-  @ApiQuery({ name: 'filter.tags', required: false, description: 'List of tags to filter', example: 'tag1,tag2,tag3' })
   @ApiQuery({ name: 'filter.orgs', required: false, description: 'List or organizations to filter', example: 'lightside,darkside' })
   @ApiQuery({ name: 'filter.teams', required: false, description: 'List or teams to filter', example: 'protected-team,private-team,public-team' })
   @ApiQuery({ name: 'filter.people', required: false, description: 'List or persons to filter', example: 'palpatine@kyso.io,rey@kyso.io' })
+  @ApiQuery({ name: 'filter.tags', required: false, description: 'List of tags to filter', example: 'tag1,tag2,tag3' })
+  @ApiQuery({ name: 'filter.files', required: false, description: 'List of file extensions', example: 'csv,tsv,md' })
+  @ApiQuery({ name: 'orderBy', required: false, description: 'Field to order by', example: 'name,createdAt,updatedAt' })
+  @ApiQuery({ name: 'order', required: false, description: 'Order', example: 'asc,desc' })
   @ApiNormalizedResponse({ status: 200, description: `Search results`, type: Tag, isArray: true })
   public async fullTextSearch(
     @CurrentToken() token: Token,
     @Query('terms') searchTerms: string,
     @Query('page') page: number,
-    @Query('perPage') perPage: number,
     @Query('type') type: string,
+    @Query('perPage') perPage: number,
+    @Query('filter.orgs') filterOrgs: string,
+    @Query('filter.teams') filterTeams: string,
+    @Query('filter.people') filterPeople: string,
+    @Query('filter.tags') filterTags: string,
+    @Query('filter.fileTypes') filterFileTypes: string,
+    @Query('orderBy') orderBy: string,
+    @Query('order') order: string,
   ): Promise<NormalizedResponseDTO<FullTextSearchDTO>> {
     if (searchTerms) {
-      const fullTextSearchDTO: FullTextSearchDTO = await this.fullTextSearchService.fullTextSearch(token, searchTerms, page, perPage, type as ElasticSearchIndex);
+      const fullTextSearchDTO: FullTextSearchDTO = await this.fullTextSearchService.fullTextSearch(
+        token,
+        searchTerms,
+        page,
+        perPage,
+        type as ElasticSearchIndex,
+        filterOrgs ? filterOrgs.split(',') : [],
+        filterTeams ? filterTeams.split(',') : [],
+        filterPeople ? filterPeople.split(',') : [],
+        filterTags ? filterTags.split(',') : [],
+        filterFileTypes ? filterFileTypes.split(',') : [],
+        orderBy,
+        order,
+      );
       return new NormalizedResponseDTO(fullTextSearchDTO, null);
     } else {
-      const emptyMeta: FullTextSearchMetadata = new FullTextSearchMetadata(page, 0, perPage, 0);
-      const emptyResult: FullTextSearchResultType = new FullTextSearchResultType([], [], [], [], emptyMeta);
+      const fullTextSearchAggregators: FullTextSearchAggregators = new FullTextSearchAggregators([], [], [], [], []);
+      const emptyMeta: FullTextSearchMetadata = new FullTextSearchMetadata(page, 0, perPage, 0, fullTextSearchAggregators);
+      const emptyResult: FullTextSearchResultType = new FullTextSearchResultType([], emptyMeta);
       const fullTextSearchDTO: FullTextSearchDTO = new FullTextSearchDTO(emptyResult, emptyResult, emptyResult, emptyResult);
-
       return new NormalizedResponseDTO(fullTextSearchDTO, null);
     }
   }
