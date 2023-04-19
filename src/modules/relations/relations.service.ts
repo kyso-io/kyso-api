b011524993a48ded2c43709453c1e073b2c16e46
Daniel Andújar <daniandujar@gmail.com>
2023-04-19 09:16:03 +0200

    fix: relation inline comment with file

--- src/modules/relations/relations.service.ts
+++ src/modules/relations/relations.service.ts
@@ -8,7 +8,7 @@ const capitalize = (s: string) => s && s[0].toUpperCase() + s.slice(1);
 
 const flatten = (list) => list.reduce((a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []);
 
-const VALID_COLLECTIONS: string[] = ['User', 'Organization', 'Team', 'Report', 'ReportAnalytics', 'Comment', 'Tag'];
+const VALID_COLLECTIONS: string[] = ['User', 'Organization', 'Team', 'Report', 'ReportAnalytics', 'Comment', 'Tag', 'File'];
 
 const listKeyToVal = (data: any) => {
   return data.reduce((prev: any, curr: any) => {
@@ -110,6 +110,7 @@ export class RelationsService extends AutowiredService {
         if (collection === 'Organization') acc[model.id] = plainToInstance(Organization, model);
         if (collection === 'Tag') acc[model.id] = plainToInstance(Tag, model);
         if (collection === 'Discussion') acc[model.id] = plainToInstance(Discussion, model);
+        if (collection === 'File') acc[model.id] = plainToInstance(Discussion, model);
         return acc;
       }, {});
       return relations;
